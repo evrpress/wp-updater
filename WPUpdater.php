b534dd2548ed234c45b7da2a203c84a4413d31dd
Xaver <xaver@everpress.co>
2024-11-23 09:54:43 +0100

    prepare for 0.1.2

--- WPUpdater.php
+++ WPUpdater.php
@@ -9,7 +9,7 @@ if ( ! class_exists( 'EverPress\WPUpdater' ) ) {
 
 		private static $instance = null;
 		private static $plugins  = array();
-		private $version         = '0.1.0';
+		private $version         = '0.1.2';
 
 		private function __construct() {
 
