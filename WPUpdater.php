3665de599326d78b97e3c2b2e41d27cfa16b6292
Xaver <xaver@everpress.co>
2025-05-09 08:49:17 +0200

    updated: bump version to 0.1.5 in composer.json, README.md, and WPUpdater.php

--- WPUpdater.php
+++ WPUpdater.php
@@ -9,7 +9,7 @@ if ( ! class_exists( 'EverPress\WPUpdater' ) ) {
 
 		private static $instance = null;
 		private static $plugins  = array();
-		private $version         = '0.1.4';
+		private $version         = '0.1.5';
 
 		private function __construct() {
 
